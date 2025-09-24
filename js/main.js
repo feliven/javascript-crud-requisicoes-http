3f35a54ef3ee21f7f027c570842cdb72473a3e2b
FVS <feliven@gmail.com>
2025-09-24 13:18:12 -0300

    adiciona novos dados via formulário

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 import interfaceUsuario from "./ui.js";
+import api from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const botaoSalvar = document.getElementById("botao-salvar");
+  const botaoCancelar = document.getElementById("botao-cancelar");
+  const formulario = document.getElementById("pensamento-form");
+
+  formulario.addEventListener("submit", manipularFormulario);
+
   interfaceUsuario.exibirPensamentos();
 });
+
+async function manipularFormulario(evento) {
+  evento.preventDefault();
+  const conteudo = document.getElementById("pensamento-conteudo").value;
+  const autoria = document.getElementById("pensamento-autoria").value;
+  const id = document.getElementById("pensamento-id").value;
+
+  try {
+    await api.salvarPensamento({ conteudo, autoria });
+    interfaceUsuario.exibirPensamentos();
+  } catch (error) {
+    alert("erro ao SALVAR");
+  }
+}
--- js/ui.js
+++ js/ui.js
@@ -2,33 +2,36 @@ import api from "./api.js";
 
 const interfaceUsuario = {
   async exibirPensamentos() {
-    const ulListaPensamentos = document.getElementById("lista-pensamentos");
-
     try {
       const arrayPensamentos = await api.buscarPensamentos();
-      arrayPensamentos.forEach((pensamento) => {
-        const liPensamento = document.createElement("li");
-        liPensamento.classList.add("li-pensamento");
-        liPensamento.setAttribute("data-id", pensamento.id);
-
-        const imagemPensamento = `<img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">`;
-
-        const divConteudoPensamento = document.createElement("div");
-        divConteudoPensamento.classList.add("pensamento-conteudo");
-        divConteudoPensamento.innerText = pensamento.conteudo;
-
-        const divAutoriaPensamento = document.createElement("div");
-        divAutoriaPensamento.classList.add("pensamento-autoria");
-        divAutoriaPensamento.innerText = pensamento.autoria;
-
-        liPensamento.innerHTML += imagemPensamento;
-        liPensamento.appendChild(divConteudoPensamento);
-        liPensamento.appendChild(divAutoriaPensamento);
-        ulListaPensamentos.appendChild(liPensamento);
-      });
+      arrayPensamentos.forEach(interfaceUsuario.adicionarPensamentoNaLista);
     } catch (error) {
       alert("ERRO NA UI");
     }
+  },
+
+  adicionarPensamentoNaLista(pensamento) {
+    const ulListaPensamentos = document.getElementById("lista-pensamentos");
+
+    const liPensamento = document.createElement("li");
+    liPensamento.classList.add("li-pensamento");
+    liPensamento.setAttribute("data-id", pensamento.id);
+
+    const imagemPensamento = `<img src="assets/imagens/aspas-azuis.png" alt="Aspas azuis" class="icone-aspas">`;
+
+    const divConteudoPensamento = document.createElement("div");
+    divConteudoPensamento.classList.add("pensamento-conteudo");
+    divConteudoPensamento.innerText = pensamento.conteudo;
+
+    const divAutoriaPensamento = document.createElement("div");
+    divAutoriaPensamento.classList.add("pensamento-autoria");
+    divAutoriaPensamento.innerText = pensamento.autoria;
+
+    liPensamento.innerHTML += imagemPensamento;
+    liPensamento.appendChild(divConteudoPensamento);
+    liPensamento.appendChild(divAutoriaPensamento);
+    ulListaPensamentos.appendChild(liPensamento);
+    console.log(liPensamento);
   },
 };
 
