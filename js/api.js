a463a4dde2e7c968d6b0f297274291eeac321282
FVS <feliven@gmail.com>
2025-09-24 12:55:33 -0300

    implementado método POST na API

--- js/api.js
+++ js/api.js
@@ -8,6 +8,20 @@ const api = {
       throw error;
     }
   },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const response = await fetch("http://localhost:3000/pensamentos", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(pensamento),
+      });
+      return await response.json();
+    } catch (error) {
+      alert("ERRO NA API");
+      throw error;
+    }
+  },
 };
 
 export default api;
