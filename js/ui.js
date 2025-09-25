f78f2421c37c891be1eda7784c562e2aae625294
FVS <feliven@gmail.com>
2025-09-25 09:29:03 -0300

    implementados botões Editar e Apagar

--- js/ui.js
+++ js/ui.js
@@ -27,9 +27,32 @@ const interfaceUsuario = {
     divAutoriaPensamento.classList.add("pensamento-autoria");
     divAutoriaPensamento.innerText = pensamento.autoria;
 
+    const iconeEditar = document.createElement("img");
+    iconeEditar.src = "/assets/imagens/icone-editar.png";
+    iconeEditar.alt = "Editar pensamento";
+
+    const botaoEditar = document.createElement("button");
+    botaoEditar.classList.add("botao-editar");
+    botaoEditar.onclick = () => interfaceUsuario.preencherFormularioComPensamentoSalvo(pensamento.id);
+    botaoEditar.appendChild(iconeEditar);
+
+    const iconeApagar = document.createElement("img");
+    iconeApagar.src = "/assets/imagens/icone-excluir.png";
+    iconeApagar.alt = "Apagar pensamento";
+
+    const botaoApagar = document.createElement("button");
+    botaoApagar.classList.add("botao-apagar");
+    botaoApagar.appendChild(iconeApagar);
+
+    const botoesPensamento = document.createElement("div");
+    botoesPensamento.classList.add("icones");
+    botoesPensamento.appendChild(botaoEditar);
+    botoesPensamento.appendChild(botaoApagar);
+
     liPensamento.innerHTML += imagemPensamento;
     liPensamento.appendChild(divConteudoPensamento);
     liPensamento.appendChild(divAutoriaPensamento);
+    liPensamento.appendChild(botoesPensamento);
     ulListaPensamentos.appendChild(liPensamento);
     console.log(liPensamento);
   },
@@ -37,6 +60,17 @@ const interfaceUsuario = {
   limparFormulario() {
     const formulario = document.getElementById("pensamento-form");
     formulario.reset();
+  },
+
+  async preencherFormularioComPensamentoSalvo(idPensamentoSalvo) {
+    const pensamento = await api.getPensamentoPorID(idPensamentoSalvo);
+    const idNovoPensamento = document.getElementById("pensamento-id");
+    const campoNovoConteudo = document.getElementById("pensamento-conteudo");
+    const campoNovaAutoria = document.getElementById("pensamento-autoria");
+
+    idNovoPensamento.value = pensamento.id;
+    campoNovoConteudo.value = pensamento.conteudo;
+    campoNovaAutoria.value = pensamento.autoria;
   },
 };
 
